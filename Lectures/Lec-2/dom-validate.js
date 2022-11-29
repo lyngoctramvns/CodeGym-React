57e3d785298801ccd7e936ec0421cc29885dd894
Ly Ngoc Tram <tramln@bsscommerce.com>
2022-11-30 00:08:46 +0700

    email validate

--- Lectures/Lec-2/dom-validate.js
+++ Lectures/Lec-2/dom-validate.js
@@ -1,5 +1,18 @@
-let regex = 
+let regex = /^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)$/;
 
 function checkEmail(str){
+    if(regex.test(str)){
+        return true;
+    } else {
+        return false;
+    }
+}
 
+function checkSubmit(){
+    let email = document.getElementById('email').value;
+    if(checkEmail(email) === true){
+        alert('Email correct!')
+    }else {
+        alert('Email is invalid!')
+    }
 }
