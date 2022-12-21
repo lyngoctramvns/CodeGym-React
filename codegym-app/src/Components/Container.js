d4ae68547a102bbc150855d2a31f540da04e9e43
Ly Ngoc Tram <tramln@bsscommerce.com>
2022-12-21 07:33:21 +0700

    codegym app

---
+++ codegym-app/src/Components/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Container extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <h2>Container</h2>
+    }
+}
+
+export default Container;
---
+++ codegym-app/src/Components/New-product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NewProduct extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <h2>New Product</h2>
+    }
+}
+
+export default NewProduct;
